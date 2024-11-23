95abb1e3ddffcd047c6ab0337f0029ab87d0d983
oooccceeeaaannn <ocean18071@qq.com>
2024-11-23 15:18:05 +0800

    Fixed undoing after a past replay causing the game to  error

--- Lua/mod_logic/!settings.lua
+++ Lua/mod_logic/!settings.lua
@@ -1,2 +1,2 @@
 --When set to "true", rules made by "logic" and by "text" will be marked with a "(t)" or "(l)" in the rules list, and will the coloured accordingly
-SPLITRULETYPES = true
+SPLITRULETYPES = false
