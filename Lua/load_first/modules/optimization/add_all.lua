bdeabbce04810715c3a1ce7f09bcff02a6619a07
oooccceeeaaannn <ocean18071@qq.com>
2024-12-06 21:20:15 +0800

    drum

--- Lua/load_first/modules/optimization/add_all.lua
+++ Lua/load_first/modules/optimization/add_all.lua
@@ -3376,7 +3376,6 @@ if not NODE_LEGACY_PARSING then
     add_node(true,"nil_bump_spread", -1, {6, 1}, {2, 4})
 end
 
-dirnames.dir = {"right", "up", "left", "down"} --use insertation method to avoid overwriting
 
 table.insert(nlist.full, "event")
 table.insert(nlist.short, "event")
@@ -6479,45 +6478,5 @@ editor_objlist["text_visit"] =
     colour = {0, 2},
     colour_active = {0, 3},
 }
-
-local layers = 1
-local meta_objs = {}
-for num = 1, layers do
-    table.insert(objlistdata.alltags, "metatext (" .. num .. ")")
-    for i, v in pairs(editor_objlist_order) do
-        if string.sub(v, 1, 5) == "text_" then
-            local data = editor_objlist_reference[v]
-            if data ~= nil then
-                v = editor_objlist[data]
-                local thissprite = nil
-                if v.sprite == nil then v.sprite = v.name end
-                if v.sprite ~= nil then
-                    thissprite = v.sprite
-                    if MF_findsprite(string.rep("text_", num) .. v.sprite .. "_0_1.png", false) then
-                        thissprite = string.rep("text_", num) .. v.sprite
-                    end
-                end
-                local new = {
-                    name = string.rep("text_", num) .. v.name,
-                    sprite = thissprite,
-                    sprite_in_root = false,
-                    unittype = "text",
-                    tags = { "text", "abstract", "metatext (" .. num .. ")" },
-                    tiling = v.tiling,
-                    type = 0,
-                    layer = v.layer,
-                    colour = v.colour,
-                    colour_active = v.colour_active,
-                }
-                table.insert(meta_objs, new)
-            end
-        end
-    end
-end
-
-for i, v in ipairs(meta_objs) do
-    table.insert(editor_objlist_order, v.name)
-    editor_objlist[v.name] = v
-end
 
 formatobjlist()
--- Lua/mod_event_node/glossary.lua
+++ Lua/mod_event_node/glossary.lua
@@ -109,10 +109,220 @@ if ZgCG.ld_slcd_jvKcpSRA_MiQPjnU then
             eFedLt_EvoP = "rNcz dfMbBoJN",
             mMuWtBR_ZVbZMgm = {"tJdmq_LqYxf"},
             uAHbQg = "YYUdxEkY dMZpv",
-            PJqAnaWGGfo = [[Sduor, ONvsDE.
-        QgZr, eS3uor, ONWgWNQ.
-        sOreF, LNIgPyv, SduBX3.
-        u3XOrc, X31cS3Jc.]],
+            PJqAnaWGGfo = [[sPfBSW6Igx, 1csqfizLNA, jDEwzRfQYx, 6AeOJemDSc, eqzwgRf, ZSDEJSY6MX, PsMgLf, wF0kHp, n6<Yd2.
+        Ubdu0ed, 2MgRycsqf, izLNAj, DEwzRfQYxH, vsDHtrL6vs, D1ep>sG, gYfimDHizq, <nsdcEeyIP, sDVwFR.
+        <Qsq<nSprB, zR<QzDfiDn, Vb>TE2D>fd, Yduor4Tes, qfizLNAjDE, wzRfQY.
+        xHvsDH, tr4ncsR<, QFx>egqo5S, DKcFW2wSxw, MgOuorq, 6wFW6ZgWw, QsYswg<o.
+        izLwQsd2, AF3u3rcIJs, YHnFRoVg, qfxsDEGFW6, Vgq>cbdu3r, cIQgxHMgO, 2JsDEQ.
+        mYHnr4ncjW, nesRoZrqJQ, zDVIzO2, Ig32IFq<IF, PavzDVIzLa, Irq6AOP6M.
+        SREwXRwBFx, fZzOMJs, DEQmYHnXO2, 0gRwnXR, sIjqfJeyIw, gRyemDS.
+        ceOHJsDEQ, mYHnr41caO, JczqMwgcIR, gWrcmdE0, gRwnmDycmD, 1cmY2Mm.
+        YNieq6wgqE, IFWyIr, q6AOREASx<, QrLfBmYycb, d2kgDSBgRf, WpxNCs, DHneLfBmY6, IsOJegqo5S, DKcSiT.
+        QSircbd2Ps, Y65gxEAzYr, vzDVIzq, wJeyIQgxHM, gO2KgYfQzO, EigWfBsOuo, rqHvsDHts.
+        DsRsDHnmqw, izqoZjdc3s, qfizLNAjD, EwzRfQr3Je, mDSceLH, KsDHISDKcG, 5ncrRwBs.
+        RwBmY6Vr3J, cSDVJrOMiF, qf5mD<QrL1, orONwgY2nj, drIrq<, BsOuvFW.
+        2wSxwM, gO2Ubdu3SR, oBzYa3ed2n, mqfBOJ0qYW, 2MFP6ISxEw, FZc3SREIgR, F3XLfBmYyB, zR<QzDfiD0, Myp0Hz.
+        XLfBmYyBzR, <QzDfiD0, wyp0HzX4TK, rOvcFq00gL, yQSiTQSirQ, adKEeyIwgL, HwmDSce.
+        LHKsDHISDK, cbpncrR, NAgPfir3Jc, zqMwgcI, H6woKSYNn, mDHQsY.
+        avrPzIg3, rQzDVIzOVx, SDE0sYHgD<, 2b>0nQzDVI, zOVxSDE0sY, HgDf2b>0nQ, apuce32.
+        KgYfQzOKn, X4TQadKEey, IwgRyes, qfQsY6, weLfBmY6Is, OEBmDKQg.
+        RwQXL6Zz, D>IORfBsuI, wgRyem, DSceLHKs, DHISDKcbpn, crRwBsR, wBmY6Vr3J.
+        czqMwgcII, s3uvdT<4d0, oNpJsNpJw>, DduUbduZa4, uIrL6vsD1e, zWNIzqfn.
+        sYMneq, EMgRznsYMn, eONIgRzMgD, fGmDVRmDVI, zqfQgxo, Kr3JQaO, EiSWNwsDVW, rOvcaO10r.
+        OncaprQFxH, ZsDfBmOuCr, 4uBHdutr4S, KX4uQzLN0s, dKiXL6Zz, D>Qjiy, QaYnQaZJed, T<4d0oNpJs, NpJw>D.
+        duor4uep>s, GFqEMj, YHAzDVJeON, IgRsIgRw, njdrIOREA, Sx<Qrqwi.
+        mDzBmDycbd, 2H6woiFqf5, mD<QSWNwSY, 6weONpFqf5, mD<QYxwBsR, wBmY6Vr3Je, gqo5SD, KcmYHIsx1c, bd2kgD.
+        SBgRfW, pxNCsDHn, eqwimDzBmD, yIORwimDzB, XPccbd2iSW, NwsDVWrOvc, aO10ORwimD, zBXPJcbd2i, SWNwsD.
+        VvrOvc, aO10rO, ncaiSemYHI, sx1Bgq<VsY, rcbduZOJ0q, YWHwzqHA, gqo0F3, MIFxwPgR, wJX4yQ.
+        adJesDVJOR, fQFxfIs3uv, FW2wSxwMgO, uobdu3zqoA, SxokFqEwj, OrIrL6vsD1, ezWNIz, qfnsYM.
+        neqEMgRzns, YMneONIgR, zMgDfGz, qoASxokFqE, wjOrIX4uQF, xHZsDfBzZu, Cr4uBHdukr, 4TZXLH, 5FRfwg.
+        Rcce3uK, X5>QaOEnFP, fwX4aQzLN0, sdElHOKEGd, KieyIry, >HXYnw76, Jw7df6sr4n, cauIH6, woKgq<.
+        VFxo0gRyvr, RwBsRwBmY6, Vr3JesDEis, DwRrOMiFq, f5mD<Qr4no, rON5zYHn.
+        gxn3ed2nmq, fBORwRr, q6wFW6Z, gWwGgDfiFx, <Psduobd2B, mDKczqMwgc, IJsYHn, FRoVYx.
+        0wFWHMsx>c, bdu3zx<IzO, 2Wmq<nr, cIwgRyezWN, IzqfnsYMne, q6wFW6ZgW, wGgDfi.
+        Fx<PsdK, KXLH5FRfw, gPFce3uKX5, >cXd2izL, NIgRFBgq, fBeq6wFW6Z, gWwGgDfi, Fx<PsdJceZ, uEHdEi, SWNwsD.
+        VvrOvcaO10, X4uQzLN0sd, KiXL6ZzD>Q, jiyQaY, nQaZJedT<4, d0oNpJsNpJ, w>Dduor4ue, p>sGFqE, MjYHAz.
+        DVJeONIgRs, IgRwnj, drIORf, QFxfIs3u, vFW2wSxwMg, Ouobdu3.
+        SRoBzYa3ed, 2nmqfBOJ0q, YW2QSYwigW, fBsOc3SRoB, zYa3eyIwgR, yep>sGF, RfkgWs, wSREASxkw.
+        sRswSWyvaO, JezY2JSY6w, SxoJsduo, r4TesR, fMzLfZsYac, bd2lGy, IRsD<nzYNw, mDVJsYccbd, 2lGyIx.
+        mYH0SDE, RsD<nzYNwF, ZuorLkoORV, AzqswSY60F, Rfir4ncjWn, eSxoQgq.
+        f5zqzMF, RNMsx>veyI, wgLHwmDSce, qzwgRfZSDE, JSY6MXPsMg, LfwF0kHpn6, <Yduobdu.
+        0ed2nmqfBO, P6IgDfJgDf, iFx<Psdc3, 6qfizLNAjD, EwzRfQeOJ, cSx<Qg.
+        qfJrqsZgxn, csD6IzqoZX, 32dsY2AFPy, czqMIFZT3e, yIwgRyesDV, JORs0gRHnm, DoBrq6wFW6, ZgWwQsYswg, OMiFqf.
+        5mD<QYZKcg, DfiFx<PsdJ, esqfizL, NAjDEwzRfQ, YxHvsDHtr, 4nczLN, 0syIJsYH.
+        nFRoVgqfxs, DEGFW6Vgq, >cbd2iFq, f5mD<QYZ2A, F3u3rcIJsY, HnFRoVYx0w, FWHMsx>cbd, u3rcIIs3uv, sqfizL, NAjDEw.
+        zRfQYWHnjD, Ewr4norO, NIgRsIgRwn, jdrIrqoZrO, MJsYHnFR, oVgqfx, sDEGFW6V, gq>cbpncr.
+        P6AgxHAgY, 2QsYc3ed2A, F3uvsqfizL, NAjDEwzRfQ, YWHnjD, Ewr4norO.
+        N5zYHngxn3, ed2nmqf, BOPHwzL, HAzDVJgR<k, sdc3FRfkgW, sMgOrQa, 3Jesqfiz.
+        LNAjfoksYH, iSDzwr4ncg, DfiFx<P, sd2AF3u3dd, 2ZSD1cgWfn, rqoRrqwJs.
+        D<ircIwgLH, wmDSceq6, wFW6ZgWwQs, Yswg<oizLw, Qsd2Ubdu3s, D0KzLJ3ed2, MgRyce, q6wFW6ZgWw, QsYswg.
+        <oizLwQsd2, Ubdu3SRoB, zYa3ed2nm, qfBOPH, wzLHAzDVJg, R<ksdc3FRf, kgWsMgOrQ, adJesDVJ, OJ0qYx.
+        00Fxw5FW, 6Mzq>vad, JesxfB, sYNMgq6Mzq, TZXPsMgL, fwF0k7, p02ayf, w<>wncbduE, ORfBsu.
+        IQgxHMgO2J, mYNGFLNAF, Lacbd2lG, yvkXDEASx<, Qrq6IFwoKF, RoKFZuorLQ, 3jDo0zYu3X, ONVgWfJg.
+        WzBr3K3jDo, 0gqfRzOrQr, PwAzYNIsxM, nr3K3jD, o0aPfKr3K3, jDo0aR6A.
+        zx13XONVgW, >ZgqfRzOrQ, rPwAzp, NZmDzvz, OrQrP20FxM, 0FOrQrP20F, xMJgWzBr3.
+        K3FLfi, mqEwsPy3XO, NKzYHvFRwP, mLy3XONKz, DEQzYu3XON, KzDEQsqoWg, 3rQrP20gqE, QsDsnr3K, 3FLfQg.
+        LNIsxMnr3K, 3gDoZsYfKr, 3K3gDoZ, sD6Azx13XO, NkgWNwgqfR, zOrQrR0AFR, fZmDzvzO.
+        rQrPHngW20, FOrQrPHngW, 2JgWzBr3K3, FW6AFqEwsP, y3XONizqoK, FRwPmLy.
+        3XONimqw, RzLfKr3K3F, xMIsP6J, gWzBr3, K3FxMIsP6Q, sDsnr3K, 3FxMIs.
+        P6ZmDzv, zOrQrPHWSY, 20FOrQrPH, WSY2JgWzBr, 3K3FWzMFqE, wsPy3XON.
+        izx<KFRwPm, Ly3GyIQgxH, MgO24>J, <pdTw7, 60oH6fHpy>, z<>Zuo.
+        rLQcgxVGSY, 2KsD<ZSD, V5sduorLQe, DZNjsxE, VFqMGrwncb, du3fTobrTM, 2>JycfT8c>, T<d>n>Mr3K, eDZNjg.
+        RoJsf83, YduorON>pn, 8cdT<d6, O2>pZ2yyf, Np6dT3XuI, grwVwzRfB, z<83Yd, uorON>.
+        pn8cdT<d6O, 2>pZ2yyfNp, 6dT3XuIgrw, VQgxzIS083, YduorON>pn, 8cdT<d6O2>, pZ2yyfNp6d, T3XuIgrwVn, sYMnYW, 6wjL6G.
+        NONzr4, ncrw6bpZ2H, 6f62rdrQOw, Q3YP6wjL6G, sxEVFq, MGNONzr4nc, rw6bpZ2.
+        H6f62rdrQO, wQ3YP6wj, L6GgRoJsf8, Jrwncbdu3f, TobrT0<f, TTMr3KeDZN, jzqf1z<ow, zRfBz<.
+        8Jrwncbdu3, fTobrT0<f, TTMr3KeDZN, jzqf1z<oQg, xzIS08Jrwn, cbdu3fTo, brT0<fTTMr, 3KeDZN.
+        jzqf1z<o, BzDM0m, ONzr4n, crw6bpZ276, >z2fTwD6dT, 3XuIgr.
+        wVnsYMnYW6, 0FRVIgRzGg, PfvzDc, Jrwncbd, u3fTobrT, V<6n<>, dfs<rdrQOP, nQORoB, YW2MFP.
+        HIgRFcbd2, lOwQ3SPN, MgRHvmDVP, YZNzr4nc, rw6bpZ2Hf>, Hrr<22.
+        >wHNpJFcdJ, <7dZT3XuIg, rP2IzRonYZ, Nzr4ncrw, 6bpZ2Hf>Hr, r<22>wHNpJ, FcdJ<7dZT3, XuIoXu, IIgwoZ.
+        zDEwFZuor, LQeDZNjsxE, VFqMGrwncb, du3fTob, rTM2>Jycf, T8c>T<d, >n>Mr3KeDZ, NjgRoJ, sf83Yd.
+        uorON>pn8c, dT<d6O2>, pZ2yyfNp, 6dT3XuIgrw, VwzRfBz, <83YduorO, N>pn8cdT<.
+        d6O2>pZ2yy, fNp6dT3XuI, grwVQgxzIS, 083YduorON, >pn8cdT<d6, O2>pZ2yyf, Np6dT3X, uIgrwV, PgLwKm.
+        Oy3Ydu, orON>p, n8cdT<d6O2, >pZ2yyfNp6, dT3XuIgrwV, Bgx6wNONzr, 4ncrw6bpZ2, ryfNTr, <6br<2.
+        2>wH<rdrQO, wQ3YRfxsDV, nNONzr4ncr, w6bpZ2ryfN, Tr<6br<, 22>wH<r.
+        drQOwQ3YRE, Asxw5NONzr, 4ncrw6b, pZ2ryfNTr<, 6br<22>wH<, rdrQOwQ3YP, HVgDNAgOy.
+        3YduorON>p, n8cdT<d6O2, >pZ2yyfNp6, dT3XuIgrwV, QgxFJrwnc, bdu3fTobrT, M2>JycfT.
+        8c>T<d>n>M, r3KeDZN, jmDViSWNIS, R>Jrwncbdu, 3fTobrTM2>, JycfT8c.
+        >T<d>n>Mr3, KeDZNjmD0K, FRwBzO, y3YduorON>, pn8cdT<d6O, 2>pZ2yyf.
+        Np6dT3XuI, grwViSWN, ISRNQsdy3Y, duorON>pn8, cdT<d6O, 2>pZ2y, yfNp6d, T3XuIgrw.
+        VQgxFJrwn, cbdu3fTobr, TM2>JycfT8, c>T<d>n>M, r3KeDZNjzq, f1z<83Yduo, rON>pn, 8cp>f>ydT3, XuIgrw.
+        VPgLwKm<83, YduorON>, pn8cp>f>yd, T3XuIgrwVB, gx6wYZNz, r4ncrw6.
+        bpZ2H6f62r, drQOwQ3YRf, xsDVnYZNzr, 4ncrw6bpZ2, H6f62rdrQO, wQ3YRE.
+        Asxw5YZNzr, 4ncrw6bp, Z2H6f6, 2rdrQOwQ3Y, R0wzqT3Ydu, orON>pn8cp, >f>ydT3XuI, grwV0gR0.
+        wzqTJrwncb, du3fTobr<f, 7p>f>ydT3X, uIgrwVksDz, MNONzr4, ncrw6b, pZ2H6f6.
+        2rdrQOw, Q3YR0wsDTJ, rwncbdu3, fTobrT, 0<fTTMr3K, eDZNjgDf.
+        BSdy3Yd, uorON>p, n8cp>f>ydT, 3XuIgrwVks, DEMNONzr, 4ncrw6.
+        bpZ2H6f62r, drQOwQ3YPf, BgDfPSdy3Y, duorON>p, n8cf>VH6f6, 2rdrQOw, Q3YPfBg.
+        DfwSdy3, YduorON>pn, 8cf>VH6f62, rdrQOwQ, 3YPfBgDfBS, dy3YduorON, >pn8cf>VH6, f62rdr, QOwQ3Y.
+        PfBgDfQSdy, 3YduorON>p, n8cf>V, H6f62rdrQO, wQ3YP6vm, YaJrwncbdu, 3fTobrTM2>, JycfT8cd, >0ypTf.
+        H6>V>rdrQO, wQ3YP6vSYy, Jrwncbdu3f, TobrTM2>Jy, cfT8cd>0yp, TfH6>V>.
+        rdrQOwQ, 3YP6vsYHwN, ONzr4ncrw6, bpZ2ryfNTr, <6brTwH>TE, <p>f7fOT3X, uIgrwV, nmqoisdy3Y, duorON.
+        >pn8cdT<d6, O2>pZ2Npf2, a6>0<pwyMr, 3KeDZN, jFW6MSRE, wNONzr, 4ncrw6bp.
+        Z2ryfNTr<6, brTwH>, TE<p>f7fO, T3XuIgrP60, FRVIgRzG, rwncbdu3fT, obrT6NDwIs, rdrQOwQ3Y.
+        RH0zOy3Ydu, orON>pn8cp, ><7Dd2y, p0Hpd>N, a6d2Yp0NT>, ZT3XuIgrw, VKSDHt, NONzr4ncrw, 6bpZ24, pn0yy>.
+        H>rdrQOwQ, 3YRz0S, YNJNONzr4n, crw6bpZ2Np, JVbynf, 7fOT3X, uIgrwV0mqc, Jrwncb, du3fTo.
+        brdrQOwQ3Y, RMkgdy3Ydu, orON>pn8Mr, 3KeDZNjsDE, QmY2imYaJr, wncbdu3fTo, brdrQOwQ.
+        3YPHANONzr, 4ncrw6b, pZT3XuIgrw, VMSW60S, DEQjdy3Ydu, orON>pn8Mr, 3KeDZNjFRf, MgqEVNONzr, 4ncrw6.
+        bpZT3X, uIgrwVWsDE, QNONzr4, ncrw6bpZT3, XuIgrwV, AmOy3Yd, uorON>pn8M, r3KeDZNjgx, QJrwnc.
+        bdu3fTobrd, rQOwQ3YPwt, gRoWNON, zr4ncrw6bp, ZT3XuI, grwVQmDk, wNONzr4ncr, w6bpZT3Xu, IgrwVC.
+        zYHnNONz, r4ncrw6, bpZT3XuIg, rwVkgDnJrw, ncbdu3fTob, rdrQOwQ3, YR<vNON.
+        zr4ncrw6bp, ZT3XuIgrw, VigqwKNONz, r4ncrw6bpZ, 2ppTwy><JM, r3KeDZNjFx, EIsq>Jrwn, cbdu3fTobr, <6bpZ2.
+        ppTwy><JMr, 3KeDZNjF, W6AFLaJ, rwncbdu, 3fTobrTNNy, fH<6OT, 3XuIgrwVKz, YHvsYa.
+        Jrwncbdu3f, TobrTN, NyfH<6OT3X, uIgrwVKz, DEQFZy3Ydu, orON>pn8cy, Jw2>nfTr, drQOwQ, 3YPHIg, RkiNON.
+        zr4ncrw6bp, Z2Od><p6>y, Mr3KeDZNjs, qfRsD<nFZy, 3YduorON>p, n8cyJw.
+        2>nfTrdrQO, wQ3YRoKsDV, iNONzr, 4ncrw6bpZ2, Od><p6, >yMr3KeD, ZNjFxM, IsP6iNO.
+        Nzr4ncrw6b, pZ2Od>, <p6>yMr3Ke, DZNjFWzM, FLaJrwncbd, u3fTob.
+        rTNNyfH<6O, T3XuIgrwVk, sDEnFZy3Y, duorON>, pn8cyJw2>n, fTrdrQOwQ3, YRMMzqfiNO, Nzr4ncrw.
+        6bpZ2Od><p, 6>yMr3K, eDZNjgqwts, YaJrwn, cbdu3fTobr, TNNyfH<6.
+        OT3XuIgrwV, QSYIVNONzr, 4ncrw6bpZ2, Hf>HrrT, 0bfJfH6>V>, rTI2pJQMr3, KeDZNj.
+        gxVwzx<VNO, Nzr4ncrw6, bpZ2Hf>Hrr, T0bfJfH6>, V>rTI2pJ, QMr3KeDZ.
+        NjSxoKjdy3, YduorON>pn, 8cpff4dO2H, p0s<p>f7f, O2ey>VXrdr, QOwQ3YP6AF, L2Qsdy.
+        3YduorON>p, n8cpff4dO2, Hp0s<p>f7f, O2ey>VX, rdrQOwQ3YR, EMzDV5mOy3, YduorON, >pn8cpff4d, O2Hp0s.
+        <p>f7fO2ey, >VXrdrQOwQ, 3YPHnmD, Htjdy3Yduo, rON>pn8cpf, f4dO2Hp0s.
+        <p>f7fO2, ey>VXrdrQO, wQ3YPHIs, qftmDHtNON, zr4ncrw6, bpZ2pd.
+        >6<fn<s>ZT, 3XuIgr, wVkgxoB, zx<QmZy, 3YduorON>p, n8c>nwHd>E, 2>32>pZ2d6, fs<>wH.
+        <rdrQOwQ3Y, PIAgxn, Jrwncbdu3f, TobrTs2>0y, Mr3KeD, ZNjjDfwzOy, 3Yduor, ON>pn8c, 6J<pfO.
+        T3XuIgrwVJ, FPfBmZ, y3YduorO, N>pn8cd>Ea, 6>z2pOT3Xu, IgrwVJFP, fBmxfZNONz, r4ncrw6bpZ, 2NpTE<, 6n<ard.
+        rQOwQ3YP, 2ZmDVnNON, zr4ncr, w6bpZ2Hf>H, rr<6<D<yMr, 3KeDZN.
+        jFxHZSY, zQNONzr4nc, rw6bpZ2Hf>, Hrr<6<D<yM, r3KeDZNjgR, o0gR6ANONz, r4ncrw6bp, Z2Hf>H, rr<f76.
+        T8cdJ<7, dZT3XuIgr, wVZsYHw, zOy3YduorO, N>pn8cf>Vq, p0NLdfsNpJ, Fc6JodrT0N, >062dn, fprdrQ.
+        OwQ3YRVA, FRfisYyJrw, ncbdu3fTob, r<f76Jod6n, wDd>VLrTsb, >32HdfH>.
+        y>k<>ZT3Xu, IgrwVKsYNi, mYHnNONz, r4ncrw6bpZ, 2Hf>Hr, r<HHf>zLpT, w76ZT3.
+        XuIgrwV3SY, HwFPfQsdy3, YduorON>p, n8cpff4dO, 2ppffL, 6nENpJFMr3, KeDZNjFq<i, zOy3YduorO, N>pn8c.
+        pJopfT<a6n, w4rdrQOwQ, 3YPHnS, YNnFZy, 3YduorON>, pn8cpf.
+        f4dO2p>TEN, f<6NpJFM, r3KeDZ, NjSxoBzq, <Ig3y3Y, duorON.
+        >pn8cp, ff4dO2p>TE, Nf<6NpJFMr, 3KeDZNj, sDVJFZy3Y, duorON>pn8, cpff4dO2p>, TENf<6N.
+        pJFMr3KeDZ, NjzLNMFxcJ, rwncbdu3, fTobrT0fy, ncc6n<d, yJ<L6dT3, XuIgrwVWSY, Hnsdy3Y.
+        duorON>, pn8cpff4, dO2LyfNOy>, z<rdrQOwQ3, YRN0mDEJNO, Nzr4ncr, w6bpZ2H, f>HrrTz2, >JN26n.
+        >Mr3KeDZNj, FxfBFxwBsZ, y3YduorON>, pn8cpff4dO, 2LyfNOy>z<, rdrQOwQ3YR, fnsYNBSDK.
+        Jrwncbdu3f, TobrToD6fN, yp0z<>, JfTrdrQOwQ, 3YPHwSWfZs, dy3YduorON, >pn8cp0s<>, w2bfnfd6>, yMr3Ke.
+        DZNjsPfnzY, NwNONzr4nc, rw6bpZ2, qff6f>Jw, pfTw4rdrQO, wQ3YP2MFW6, KFRoKNONzr, 4ncrw6bpZ.
+        27p0H>y>E, Ld>aMr3K, eDZNjFLNwF, xfBzOy3Ydu, orON>pn8cf, >VNpw6<>Jf, pfTw76ZT3X, uIgrwVt.
+        sDfKNONzr4, ncrw6bpZ2H, f>Hrr<HHf, >zLpTw76, ZT3XuIgrwV, ZsDEASDyJr, wncbdu3fTo, br<f76, Jod6nw.
+        Dd>VLrTsb, >32HdfH>y>, k<>ZT3Xu, IgrwVZsY60, FR1Jrwncbd, u3fTobr<f7, 6Jod6nwDd>, VLrTsb>32, HdfH>y, >k<>ZT.
+        3XuIgrwViF, qwBrwncbdu, 3fTobrT6ND, wIsrdrQOw, Q3YR6wFW2I, g3Nzr4ncr.
+        w6bpZ2TdfI, mDdT3XuI, grwVMSWyJ, rwncbdu3, fTobrTs, 2dn>Mr.
+        3KeDZN, jFqfZsRoZg, dy3YduorON, >pn8c6J<X6, dT3XuIg, rwVxmYN0F.
+        Zy3YduorON, >pn8cpff4d, O2Od>oryfI, 2>JyMr, 3KeDZNjjDo, 0NdQJrwn.
+        cbdu3fTobr, TEbpJFcp>o, D6>0<pwyMr, 3KeDZNjFLf, KFqfnNONz, r4ncrw6bpZ, 2pyn<dDdT, 3XuIgrwV, xgxEnN.
+        ONzr4nc, rw6bpZ2, Hf>Hrr, Tfa6>H>>, Jw4df6s, rdrQOwQ3Y, PNwsRfZFZ.
+        y3YduorON, >pn8cpff4d, O2d6>s<, >Jf7yn>, Mr3KeDZNjg, xVwNONzr4n, crw6bp.
+        Z2Hf>HrrTV, fp>N<>waMr, 3KeDZNjzL, zANONzr4nc, rw6bpZ2H, f>HrrTVfp>, N<>waMr.
+        3KeDZNjzqM, ZsD>Jr, wncbdu3fTo, brT0fynccp, wfHyJfd>ZT, 3XuIgrwVRg, WfZNONz, r4ncrw6bpZ, 2Hf>Hr.
+        rTVfp>N<>w, aMr3KeDZNj, sRwxsdy3Yd, uorON>, pn8cpff4, dO27f>, 0O6fNp.
+        rdrQOwQ3YR, fEzD<Q, NONzr4ncrw, 6bpZ2Hf, >HrrTH2, >J6NpJ<a>Z, T3XuIgrwVQ, sYHizqMMg3, y3Yduo.
+        rON>pn8cpf, f4dO24yfNT, d>V2p<aMr3, KeDZNjsWNw, SY6wFP6vSD, 1Jrwncbdu3, fTobrT0fyn, ccyn<d6Tw.
+        7y>Epr, drQOwQ3, YP2Azxf, ZNp8Jrwncb, du3fTobrT<, HyJwLf>of>, ZT3XuIgr, wVKgWzwFRf, JNp8Jr, wncbdu, 3fTobr.
+        T<HyJw, Lf>of>ZT3X, uIgrwVAFRw, PmDVMgOy3Y, duorON>pn8, cpff4dO2b>, JwLd>V2.
+        pTw>Dd, T3XuIgrwV, nFRwPsxfZs, DyJrwncbdu, 3fTobr, TNff<6, bp30ad>.
+        k<rdrQ, OwQ3YREwzR, fQsDyJr, wncbdu, 3fTobrT0f, ynccpT, fD6>ENpJ, FMr3KeDZNj, Sx<BSx.
+        fQNONzr4nc, rw6bpZ2Npf, 2b>0HNy, JE<rdrQ, OwQ3YPzMzq, Hvrwncbd, u3fTobr.
+        T0fynccfJw, pd>o7rdrQO, wQ3YP6A, jqw5NONzr4, ncrw6bpZ2H, f>HrrTH, r6>0Nyn<, ardrQOwQ3Y, R<Qmx<.
+        Qmdy3Yduor, ON>pn8cpff, 4dO24dTfHd, >H2pOT3Xu, IgrwVM, FZy3YduorO, N>pn8cp><7, Dd2N6Tf7fT, w>d>fp.
+        rdrQOwQ3YR, MIsq6wg3y3, YduorON>pn, 8c>02<y, nwqd>fTrTs, <6>ENpJF.
+        Mr3KeDZNjF, qfnzqfJN, ONzr4ncrw6, bpZ2p>Tf4, d>sN6>yc6J, f<pTw76ZT, 3XuIgrw, Vizq<nmDaJ, rwncbd.
+        u3fTob, r<Hy6>, HN6Jw<6O2, q6>fad>VLr, drQOwQ3YPH, QsDfKjdy3Y, duorON>pn8, c>02<ynwqd, >fTrTs.
+        <6>ENpJF, Mr3KeDZNjF, xMIgPJJrwn, cbdu3fTobr, <Hy6>HN6Jw, <6O2q6>f.
+        ad>VLrdrQ, OwQ3YRH0F, RwAzYaJrwn, cbdu3f, Tobr<Hy6>, HN6Jw<6.
+        O2q6>f, ad>VLrdrQO, wQ3YRVAFP, 6vNONzr4nc, rw6bpZ2H, f>HrrTH2>, J6NpJ<a>Z.
+        T3XuIgrwVi, gWfnmOy3Yd, uorON>pn8c, pff4dO24yf, NTd>V2p<aM, r3KeDZN, jsD<izOy, 3YduorON>, pn8cpf.
+        f4dO24yfNT, d>V2p<aMr3, KeDZNjzxfi, zOy3YduorO, N>pn8cpff4, dO24yfNTd>, V2p<aM.
+        r3KeDZNjsq, fQsY6wNONz, r4ncrw6bpZ, 2T6fH>>wf4, fTwD6dT3Xu, IgrwVPgqwn, SxcJrwncbd, u3fTobrTz.
+        ad>HrDdT3, XuIgrw, VQgxHMgONz, r4ncrw6bpZ, 2Hf>HrrTV<, d>zryJof>J, MbpnyMr3, KeDZNjF, Ro0zqf.
+        Zrwncbdu, 3fTobrT0fy, nccd>V>6fN, 76fyMr3K, eDZNjSxMMg, RVwgONzr.
+        4ncrw6bpZ2, Hf>HrrT, w7fTfdpJf, >rdrQOwQ3Y, RoRsPHwzON, zr4ncrw.
+        6bpZ2b6JSM, r3KeDZNjzq, MZgWFJr, wncbdu3fTo, brT0fync, c6Jody.
+        n>Mr3KeDZN, jzDVizq<5m, Zy3Yduor, ON>pn8cpTw, >fTE<rTM, <d>zrfOT3X, uIgrwV.
+        xmYHIz, Oy3YduorON, >pn8c>, <fOpTw4p<J, cfJwpdf6<6, OT3XuIgrwV, MgDNIsDV, nNONzr, 4ncrw6.
+        bpZ2Hy>, VsrTNbDTf, prdrQOwQ3Y, R<QmYswNON, zr4ncN06bp, Z2yyfN, 2p<wm6>yM, NZKeDZNjzR, fiFxfQ.
+        rwncbdu3fT, obr<Hb, f>Ea6fHprd, rQOwQ3YR, MAFOy3Yduo, rON>pn8cp, ff4dO2rp02, yd>VLrd, rQOwQ3.
+        YRMAFLaJrw, ncbdu3fTob, rT0fynccdT, oy>Tw76ZT3, XuIgrwVtSY, NkSdy3Yduo, rON>pn8c, pff4dO2p.
+        d>1Mr3KeDZ, NjFxwBsPfQ, NONzr4ncrw, 6bpZ2H, f>HrrTHb, >ZuCr<6, 2p3T3XuIg, rwVZsY2wgP, yJrwnc.
+        bdu3fTob, r<f76J<N>3, T3XuIgrwVw, SxMANONzr4, ncrw6bpZ2H, f>Hrr<2, 2>wHNpJFcd, J<7dZT3Xu.
+        IgrwVwgP6w, F3y3Yd, uorON>pn8, cp><7Dd276, fFcpTw7, 6faMr3, KeDZNjz, RfvmDHQsdy, 3Yduor.
+        ON>pn8cp>, <7Dd24yfNp, rdrQOwQ3YR, NAgWHnNONz, r4ncrw6bp, Z2Hf>Hr, rTNbp0H>rd, rQOwQ3YRNA, gWHnFZ.
+        y3YduorON>, pn8cpff4, dO2OpnopfO, T3XuIg, rwVMgq, wPgRfJrwnc, bdu3fTobrT, 0fynccy>EN, 6nVH6>.
+        V>rdrQ, OwQ3YR0A, FP2vNONzr4, ncrw6bpZ2H, f>Hrr<6dy>, Vp6Jodp, ><>d>o7, rdrQOw, Q3YP6Z.
+        SYuJrwnc, bdu3fTobrT, wapTfLy>KM, r3KeDZ, NjzLNMF, LaJrwncb, du3fTobrTw, apTfLy>, KMr3Ke.
+        DZNjsD, Eisdy3Yduo, rON>pn8cp<, f2X>k<Dfzb, >J6<6OT3, XuIgrwVKsY, NRsDHnNONz, r4ncrw6.
+        bpZ2Npf2b>, 0HNyJE<rdr, QOwQ3YR0IF, WHIgRFJrwn, cbdu3fTobr, <f76Jof, pJyMr3, KeDZNjSP, fBsxfw.
+        NONzr4n, crw6bpZ2Ty, >VL6fNbffa, Mr3KeDZN, jFRfiSWfwN, ONzr4ncrw.
+        6bpZ2fpJ, Nbf>VT6>y, Mr3KeDZNjS, R<BsZy3Ydu, orON>pn8c, 6fMypTopdf, s<rdrQOwQ, 3YR6AgDwBg, Zy3Ydu.
+        orON>pn8cd, >VTf>H>dfs, <rdrQOwQ, 3YRsIgqK3Y, duorON>, pn8cp0s<>J, sap0zNp.
+        JFMr3KeDZ, NjsRoQsONz, r4ncrw6bpZ, 24pn0yy>H>, rdrQOwQ3YR, <QmDzBrwn.
+        cbdu3fTobr, T0fynccy>E, N6nVH6>V>r, drQOwQ3YRH, QSYHiNO, Nzr4ncrw6b, pZ2N>w.
+        N2fTw7, 6ZT3OPnQOP, nesPfBSW, 6Igx1c, SxMwSxk, GSDVJYxH, ZSYHve, qVMgD>QgDo, JsdJem.
+        DScgDoJsdu, obd2BmDKcz, qMwgcIkgx6, wr4ncrRwBY, WN0gqfi, rcIwgR.
+        yemDSceL, HnFRwBsZVi, zDrvgR, <ksdKEX4yI, r4norON, BgWycr3J, czqMwgcIBS, D0wr4ncF, W6ZmDV.
+        PXPH0S3MBS, D0wX4>IORf, BsuIRgWrcF, RfPsYcQg, DfiFx<Psd2, Ig32KS, DwZFZM4>, J<pdTw760o, H6fHpy.
+        >z<>0kkgx, 6wYdJcsq, 8emDSce, LHnFRwBsZ, VkSY65mOMB, SD0wXLN, wsxf1ed.
+        2Ubd2BmDKI, rL6vsD1esq, fizLNAjDEw, zRfQeON5zY, Hngxn3Xq0, wFWHMsx.
+        >IOPNwzLfZ, g32nFPfwOR, fBsuIwgRye, sRoZr<8Qsq, wZYW2Z, gWucmD1cmY, 2MmYNieq6I, FwoKFRoK.
+        FZJcsq8emD, ScgR<ksd, uobd2JmYNG, FLNAFO2nmq, fBOR6wFW, 6ZgWwQsYsw, gOc3SWfizq, okr3K3fTob, rT6NDw.
+        IsrdrIOPNw, zLfZg32nFP, fwORfBsuIw, gRyeFRfnzY, NBrqsMgLHw, rqfBsuIQgx, HMgO2Agq6G, FxfBzqHMgq, acbd25.
+        SDE5zDEMzq, fisDVnsDV5, sYaesPfBSW, 6Igx1c, Sx<QSWf, QSY6wFxf, BzqfBSxfie, O1BX3Je, gqo5SD.
+        KcFRfizDEn, XqsIgR<QF, ZEkSYMQsD1, QzR<ZmD<, nmDoBFZEis, DVnYxw.
+        JFZEBsYzRm, YNizLzA, FR6ir4n, cgxEJYW, HwgP65SDE5, eO1BX3.
+        JcsRoZ, rqJQFx, fBzO2Ig, 32IFq<, IFPavFRfiz, DEned2Jg, Z2RgWrc.
+        YZEnsYMnYx, HvzDVtrqwB, rqwKSDwZFZ, MisDVned2J, gKIQgxH, MgO2BSD, 0wr4nczqf, 1z<o5mLfBm, 0QEYyI.
+        Is3uvSxMw, SxkGSDVJYx, HZSYHveqVM, gD>QrRoBYW, 2MFPHIgRF3, edJczqMwgc, I3FRfM, mKIwgRycsD, VJrqfB.
+        suIZsY60FR, 1cFRfizDEn, XqsIgR, <QFZEkSYMQ, sD1QzR, <ZmD<nm.
+        DoBFZE, isDVnYxw, JFZEBs, YzRmYNizLz, AFR6irq, fBsuIQgxHM, gO2Agq6, GSD6Jg.
+        W2nmDoBr4n, cSD6JgW2, nmDoBO, Rs0gRHnmDo, Brq<JsqoK, zqwAg3MAFL, 6Igx1QSxoB, sLaQmD6iXL, sIFxw3.
+        gq>QgRonFP, fQsdEnSDzi, YZExmYH0SD, EAgREVYZJc, sRoZr<8Qzq, MIgRFc, mD1cmY, 2MmYNi.
+        eqoKzqwAg3, Jcsq8emD, ScSxMwSxkG, SDVJYxHZSY, HveL6vmDVP, ed2nmqfBOP, NwzLfZgcIw, gRyesDVJO.
+        RsAF32G, XqHAgRy, cmD1cmY2Mm, YNieqHAgR6, ied2JgKI, QgxHMgO, 25gxVJ, gR<ksdKcSx, oBsLsM.
+        gLacbd25gx, VJDi<zXO25, gxVJDiNzOR, wRrqHvsDH, tYx<Bs<o5F, R<imOM.
+        5gxVJgR<ks, dJczqM, wgcIZsY60F, R1esDV, JORsAF32GX, LsMgO2.
+        Ig32IFq<IF, PavSxoBsLs, MgLaIrq6AO, RwRrqHvsDH, tYx<Bs<o5F, R<imOMxSDK, IrL6vsD1eF, RfnzYNBORf, BsuIwg.
+        RyesDVJOP, NwzLfZg32A, gq6GSD6JgW, 2nmDoBeqoK, zqwAg3E5gx, VJFZEI, sLaQzRwimD, NQsdEBgW6Z, zDEwXL, 6MsWHG.
+        XLsIFWfM, gqoBgLw, Ged2wg, Ryezq<, 3gq>BmDVis, YNneq0As<o, vgxotYxs0g, RHnmDoBF0, Q3Sxok.
+        gD<Bs<, oPmYswg3Nz, XO2RzDV5zq, wAg3cIORsA, F32BSD0wXO, 20gRwn.
+        FZ2Ig32KSD, wZFZM0gRwn, gqwizLaIrq, 6AORwRrOH0, gRwnFZ2Ubd, uKrL6vsD1, emDScSxMwS, xkGSDVJYxH, ZSYHve.
+        qVMgD>QrRo, BYx<KFqfMF, R<BSx>3ed2, nmqfBOPNwz, LfZgcIwgR, yesDVJO.
+        RfBsuIwgRy, IXd0tsD.]],
             xIsiC = "GvQbPY"
         },
 
